ccd0ff3e681fa92a755f3fb97bf2cd4eb21dff27
BlueEvilGFX <107540992+BlueEvilGFX@users.noreply.github.com>
2022-08-14 21:53:13 +0200

    added comments

--- preparer/src/index.js
+++ preparer/src/index.js
@@ -1,17 +1,21 @@
+// ━━━━━━━━━━━━ import normal libraries
 const readline = require('readline');
 const async = require('async');
 
+// ━━━━━━━━━━━━ import functions from other files
 const { importAddon } = require("./importAddon");
 const { getNewestLocalVersion } = require("./localVersionGetter");
 const { removePycache, removeFiles } = require("./removers");
 const { clearDlcJson } = require("./clearDLC");
 const { zipAddon } = require("./zipper");
 
+// ━━━━━━━━━━━━ variables
 const addonName = 'MC_Assets_Manager'
 const barLength = 6;
 const bars = '━'.repeat(barLength);
 let addonPath = null;
 
+// ━━━━━━━━━━━━
 function getCmdInput(query) {
     const lineReader = readline.createInterface({
         input: process.stdin,
@@ -24,17 +28,20 @@ function getCmdInput(query) {
     }))
 }
 
+// ━━━━━━━━━━━━ main function
 async function main() {
     console.clear();
     let input = await getCmdInput('\n   > Import addon version (y) or use locally stored version (n)?   y/n: ');
 
     switch (input) {
         case 'y':
+            // ━━━━━━━━━━━━ importing and then prepping
             console.log('   > proceeding with importing...');
             importAddon();
             console.log('   > proceeding with preparing for upload...');
             prepareAll();
             break;
+            // ━━━━━━━━━━━━ only prepping
         case 'n':
             console.log('   > proceeding with preparing for upload...');
             prepareAll();
@@ -45,6 +52,7 @@ async function main() {
 
 }
 
+// ━━━━━━━━━━━━ function which executes all preperation
 function prepareAll() {
     async.series([
         function (callback) {addonPath = getNewestLocalVersion(callback, addonName, bars)[0]},
