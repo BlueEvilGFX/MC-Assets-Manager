de2357a6b2c90c96553ba4663d9c49b104910c11
marian <elias-marian@web.de>
2022-08-14 14:09:33 +0200

    General code improvements - js conventions, cleanup, ...

--- index.js
+++ index.js
@@ -1,34 +1,33 @@
 const readline = require('readline');
 
-function get_cmd_input(query) {
-    const rl = readline.createInterface({
+function getCmdInput(query) {
+    const lineReader = readline.createInterface({
         input: process.stdin,
         output: process.stdout
     });
 
-    return new Promise(resolve => rl.question(query, ans => {
-        rl.close();
+    return new Promise(resolve => lineReader.question(query, ans => {
+        lineReader.close();
         resolve(ans);
     }))
 }
 
 async function main() {
     console.clear();
-    let cmd_input = await get_cmd_input("\n   > Import addon version (y) or use locally stored version (n)?   y/n: ");
+    let input = await getCmdInput('\n   > Import addon version (y) or use locally stored version (n)?   y/n: ');
 
-    switch(cmd_input) {
+    switch (input) {
         case 'y':
-            console.log("   > proceeding with importing...");
-            import_addon();
+            console.log('   > proceeding with importing...');
+            await importAddon();
             break;
         case 'n':
-            console.log("   > proceeding with preparing for upload...");
-            prepare_all();
+            console.log('   > proceeding with preparing for upload...');
+            await prepare_all();
             break;
         default:
-            cmd_input = main();
+            input = main();
     }
-
 
 }
 
@@ -36,131 +35,110 @@ const archiver = require('archiver');
 const path = require('path');
 const fs = require('fs');
 
-const addon_name = 'MC_Assets_Manager'
-const bar_length = 6;
-const bars = '━'.repeat(bar_length);
-var addon_path = null;
+const addonName = 'MC_Assets_Manager'
+const barLength = 6;
+const bars = '━'.repeat(barLength);
+let addonPath = null;
 
-async function import_addon() {
+async function importAddon() {
 
 }
 
-
-async function get_newest_local_version(){
+async function getNewestLocalVersion() {
     let _path = path.join(path.dirname(__dirname), 'versions');
-    
-    // create dict of all available versions
-    // joines the version integers together 
-    // --> only one int per version sub int is possible!
+
+    // create object of all available versions
+    // joins the version integers together
+    // --> Versions must match x.x.x, 3 version tags each only 1 digit!
     // [{ key: '0.1.6', value: '016' }]
-    var versions = fs.readdirSync(_path).map(function(i){
-        return {key:i, value: i.split('.').join("")}
+    const versions = fs.readdirSync(_path).map(i => {
+        return {key: i, value: i.split('.').join('')}
     });
 
-    var highest = 0;
-    versions.forEach(function(item){
-        if(highest == 0){
-            if(item.value > highest){
-                highest = item;}
-        }else{
-            if(item.value > highest.value){
-                highest = item}};
+    let highest = 0;
+    versions.forEach(item => {
+        if (item.value > highest) highest = item.value;
     });
 
-    addon_path = path.join(_path, highest.key, addon_name);
+    addonPath = path.join(_path, highest.key, addonName);
 
     console.log('\n');
     console.log(bars);
     console.log(`   Newest Version: ${highest.key}`);
-    console.log(`   path: ${addon_path}`);
+    console.log(`   path: ${addonPath}`);
 }
 
-function remove_pycache(){
-    function list_dirs_pycache(_path){
-        fs.readdir(_path, { withFileTypes: true }, (error, files) => {
-            const directoriesInDirectory = files.filter((item) => 
-                item.isDirectory()).map((item) => item.name);
-    
-            directoriesInDirectory.forEach(function(name){
-                var dir = path.join(_path, name);
-                if(name != '__pycache__'){
-                    list_dirs_pycache(dir)
-                }
-                else{
-                    fs.rmSync(dir, { recursive: true, force: true });
-                }
-            })
+function removePycache() {
+    fs.readdir(addonPath, {withFileTypes: true}, (error, files) => {
+        const subDirectories = files.filter(item => item.isDirectory()).map(item => item.name);
+
+        subDirectories.forEach(name => {
+            const dir = path.join(addonPath, name);
+            if (name !== '__pycache__') listDirsPycache(dir)
+            else fs.rmSync(dir, {recursive: true, force: true});
         })
-    }
-    list_dirs_pycache(addon_path);
+    })
 
     console.log(bars);
     console.log('   removed pycache');
 }
 
-function remove_files(){
-    let files_dir = path.join(addon_path, "files");
-    let DLCs_dir = path.join(files_dir, "DLCs");
-    let o_assets = path.join(files_dir, "own_assets");
-    let o_presets = path.join(files_dir, "own_presets");
-    let o_rigs = path.join(files_dir, "own_rigs");
-	let addon_updater = path.join(addon_path, "mc_assets_manager_updater");
+function remove_files() {
+    let files_dir = path.join(addonPath, 'files');
+    let DLCs_dir = path.join(files_dir, 'DLCs');
+    let o_assets = path.join(files_dir, 'own_assets');
+    let o_presets = path.join(files_dir, 'own_presets');
+    let o_rigs = path.join(files_dir, 'own_rigs');
+    let addon_updater = path.join(addonPath, 'mc_assets_manager_updater');
 
-    fs.rm(addon_updater, { recursive: true, force: true }, (error) => {});
-
-    fs.rm(DLCs_dir, { recursive: true, force: true }, (error) => {});
-    fs.rm(o_assets, { recursive: true, force: true }, (error) => {});
-    fs.rm(o_presets, { recursive: true, force: true }, (error) => {});
-    fs.rm(o_rigs, { recursive: true, force: true }, (error) => {});
+    fs.rm(addon_updater, {recursive: true, force: true}, null);
+    fs.rm(DLCs_dir, {recursive: true, force: true}, null);
+    fs.rm(o_assets, {recursive: true, force: true}, null);
+    fs.rm(o_presets, {recursive: true, force: true}, null);
+    fs.rm(o_rigs, {recursive: true, force: true}, null);
 
     console.log(bars);
     console.log('   removed files');
 }
 
-function clear_dlc_json(){
-    let files_dir = path.join(addon_path, "files");
+function clearDlcJson() {
+    let files_dir = path.join(addonPath, 'files');
     let json_path = path.join(files_dir, 'dlcs.json')
-    fs.writeFile(json_path, '{}', function (err) {
+    fs.writeFile(json_path, '{}', err => {
         console.log(bars);
         console.log('   cleared dlc json');
     });
 }
 
-function zip_addon(){
-    var destintation = path.join(path.dirname(addon_path), `${addon_name}.zip`);
-    var output = fs.createWriteStream(destintation);
-    var archive = archiver('zip');
+function zip_addon() {
+    const destination = path.join(path.dirname(addonPath), `${addonName}.zip`);
+    const output = fs.createWriteStream(destination);
+    const archive = archiver('zip', {});
 
-    output.on('close', function () {
+    output.on('close', () => {
         console.log(bars);
         console.log('   zipped addon');
     });
 
-    archive.on('error', function(err){
-        throw err;
-    });
-
+    archive.on('error', err => throw err);
     archive.pipe(output);
-
     // append files from a sub-directory, putting its contents at the root of archive
-    archive.directory(addon_path, false);
-
+    archive.directory(addonPath, false);
     // append files from a sub-directory and naming it `new-subdir` within the archive
     archive.directory('subdir/', 'new-subdir');
-
     archive.finalize();
 }
 
-async function prepare_all(){
-    async function prep_all(){
-        await get_newest_local_version();
-        remove_pycache();
+async function prepare_all() {
+    async function prep_all() {
+        await getNewestLocalVersion();
+        removePycache();
         remove_files();
-        clear_dlc_json();
+        clearDlcJson();
     }
-    
+
     await prep_all()
     zip_addon();
 }
 
-main();
+await main();
