b0d75c04d5aecfc68564215fe9e79c92c96413b1
BlueEvilGFX <107540992+BlueEvilGFX@users.noreply.github.com>
2022-08-15 12:02:13 +0200

    Add files via upload

--- developer/src/zipper.js
+++ developer/src/zipper.js
@@ -3,11 +3,13 @@ const path = require('path');
 const fs = require('fs');
 
 async function zipAddon(callback, addonPath, addonName) {
-
+    dirPath = path.dirname(addonPath);
+    fs.rmSync(path.join(dirPath, 'MC_Assets_Manager.zip'), {recursive: true, force: true});
     const sleep = ms => new Promise(r => setTimeout(r, ms));
     await sleep(1);
     
-    const destination = path.join(path.dirname(addonPath), `${addonName}.zip`);
+    const dirUpPath = path.dirname(dirPath);
+    const destination = path.join(dirUpPath, `${addonName}.zip`);
     const output = fs.createWriteStream(destination);
     const archive = archiver('zip', {});
 
@@ -27,10 +29,13 @@ async function zipAddon(callback, addonPath, addonName) {
 
     archive.pipe(output);
     // append files from a sub-directory, putting its contents at the root of archive
-    archive.directory(addonPath, false);
+    archive.directory(dirPath, false);
     // append files from a sub-directory and naming it `new-subdir` within the archive
     archive.directory('subdir/', 'new-subdir');
     archive.finalize();
+    // moving zip 
+    await sleep(1);
+    fs.renameSync(destination, path.join(dirPath, `${addonName}.zip`));
     callback();
 }
 
