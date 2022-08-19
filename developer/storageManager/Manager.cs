80fbe8fb0c6258b7dbb63b623017fae5d55077b5
BlueEvilGFX <107540992+BlueEvilGFX@users.noreply.github.com>
2022-08-19 16:39:21 +0200

    Add files via upload

---
+++ developer/storageManager/Manager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace storageManager
+{
+    internal class Manager
+    {
+        public Printer printer;
+        public string? blenderVersion;
+        public string? application;
+
+        public Manager(Printer printer)
+        {
+            this.printer = printer;
+            blenderVersion = GetBlenderVersion();
+            application = GetApplication();
+        }
+
+        private string GetBlenderVersion()
+        {
+            string roamingPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            string blenderVersionsPath = Path.Join(roamingPath, "Blender Foundation", "Blender");
+            string[] blenderVersions = Directory.GetDirectories(blenderVersionsPath).Select(i => Path.GetFileName(i)).ToArray();
+
+            if (blenderVersions.Length == 1)
+            {
+                printer.PrintOneLiner($"Blender version: {blenderVersions[0]}");
+                return blenderVersions[0];
+            }
+
+            string instruction = "Blender version: ";
+            return printer.InputInit(instruction, blenderVersions);
+        }
+
+        private string GetApplication()
+        {
+            string[] options = { "a", "d"};
+            string[] optionDefinitions = { "<a> import addon", "<d> import dlcs"};
+
+            string instruction = "application: ";
+            return printer.InputInit(instruction, options, optionDefinitions);
+        }
+    }
+}
---
+++ developer/storageManager/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace storageManager
+{
+    class Programm
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Printer printer = new Printer();
+            Manager manager = new Manager(printer);
+            Console.ReadKey();
+        }
+    }
+}
