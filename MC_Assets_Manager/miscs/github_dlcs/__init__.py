f6956994a886c9180cf1b9c5296f1993867fac4b
BlueEvilGFX <107540992+BlueEvilGFX@users.noreply.github.com>
2023-05-05 15:31:02 +0200

    deleting code for upload 0.3.0

--- MC_Assets_Manager/miscs/github_dlcs/__init__.py
+++
@@ -1,9 +0,0 @@
-from . import icons, operators, ui, connect, auto_check
-
-def register():
-    icons.register()
-    operators.register()
-
-def unregister():
-    operators.unregister()
-    icons.unregister()
--- MC_Assets_Manager/ui_lists/ui_list_operators/__init__.py
+++
@@ -1,8 +0,0 @@
-from . import main_add_ops
-
-def register():
-    main_add_ops.register()
-
-
-def unregister():
-    main_add_ops.unregister()
